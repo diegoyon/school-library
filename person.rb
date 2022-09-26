5691bef52eceb06b487c71631eb7394630c41075
Diego Yon <diegoyon@outlook.com>
2022-09-26 11:50:56 -0600

    Create methods and variables for class person

---
+++ person.rb
@@ -0,0 +1,35 @@
+class Person
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def initialize(name="Unknown", parent_permission=true, age)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def can_use_services?
+    if is_of_age?
+      return true
+    else
+      return false
+    end
+  end
+
+  private
+
+  def is_of_age?
+    if @age >=18
+      return true
+    else
+      return false
+    end
+  end
+end
+
+diego = Person.new('Diego', 16)
+p diego.name
+p diego.age
+
+
+p diego.can_use_services?

4f31927974d41999638887e88208f6240bcc8a3b
Diego Yon <diegoyon@outlook.com>
2022-09-27 09:20:55 -0600

    prepare base decorator

---
+++ decorator.rb
@@ -0,0 +1,13 @@
+require './decorator'
+
+class Decorator<Nameable
+  attr_accessor :nameable
+
+  def initialize(nameable)
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end

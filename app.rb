c5be24e9f53639590bf9ea01210806fbb8987697
Diego Yon <diegoyon@outlook.com>
2022-09-28 11:45:59 -0600

    create menu

---
+++ app.rb
@@ -0,0 +1,20 @@
+class App
+  def list_books
+  end
+
+  def list_people
+  end
+
+  def create_person
+  end
+
+  def create_book
+  end
+
+  def create_rental
+  end
+
+  def list_rentals_of_person
+  end
+end
+

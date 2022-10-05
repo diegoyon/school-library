5095701e4677cab2941f12ae4a6c52a37288612e
Diego Yon <diegoyon@outlook.com>
2022-10-05 10:22:40 -0600

    Add book tests

---
+++ spec/book_spec.rb
@@ -0,0 +1,15 @@
+require_relative '../book'
+
+describe "#title" do
+  it "returns the correct title" do
+    book = Book.new('Lord of the Rings', 'Tolkien')
+    expect(book.title).to eql 'Lord of the Rings'
+  end
+end
+
+describe "#author" do
+  it "returns the correct author" do
+    book = Book.new('Lord of the Rings', 'Tolkien')
+    expect(book.author).to eql 'Tolkien'
+  end
+end
